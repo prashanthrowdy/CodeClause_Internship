f9b8c219ed15b2f563dfc2fb0dfdcc77cd1cc54e
Prashanth Tellakula <96677562+prashanthrowdy@users.noreply.github.com>
2023-10-31 23:40:20 +0530

    alarm.py

--- alarm.py
+++ alarm.py
@@ -5,19 +5,17 @@ import time,sys
 from PIL import Image, ImageTk
 from tkinter.constants import NW
 import os
+from playsound import playsound
 
 
 def set_alarm():
     alarm_time = user_input.get()
     while True:
         current_time = time.strftime('%H:%M:%S')
-        if alarm_time=="":
-            messagebox.askretrycancel("Error Message","Please Enter Value")
-        else:
-            if current_time == alarm_time:
-                messagebox.showinfo("Alarm", "Time to wake up!")
-                #playsound("JackSparrow_Bgm.mp3")
-                break
+        if current_time == alarm_time:
+            messagebox.showinfo("Alarm", "Time to wake up!")
+            playsound("JackSparrow_Bgm.mp3")
+            break
 
 
 
@@ -27,35 +25,30 @@ root.geometry("622x350")
 canvas=tk.Canvas(root,width=622,height=350)
 
 # Load the image and resize it to match the canvas dimensions
-image = Image.open("alarm1.jpg")
+image = Image.open("alarm.jpg")
 image = image.resize((622, 350))
-image=ImageTk.PhotoImage(Image.open("alarm1.jpg"))
+image=ImageTk.PhotoImage(Image.open("alarm.jpg"))
 
 
 # Create a Canvas widget with the resized image
 canvas = tk.Canvas(root, width=622, height=350)
 canvas.create_image(0,0,anchor=NW,image=image)
 canvas.pack()
-
 header=tk.Frame(root)
-
-#label
-label = tk.Label(root, text="Enter alarm time (HH:MM:SS):")
-label.pack()
-box3=tk.Frame(root)
-box3.place(x=200,y=150)
-
 
 
 box1=tk.Frame(root)
-
 box1.place(x=250,y=180)
-
 box2=tk.Frame(root)
+box2.place(x=265,y=230)
 
-box2.place(x=250,y=260)
+#label
+box3=tk.Frame(root)
+box3.place(x=220,y=120)
+comment=tk.Button(box3,text="Enter alarm time (HH:MM:SS):",font=('Arial Narrow',12))
+comment.grid(row=0,column=2)
 
-#Time taken by User as Input
+#Input Time
 
 user_input=tk.Entry(box1,font=('Arial Narrow',20),width=10)
 user_input.grid(row=0,column=2)
@@ -63,7 +56,6 @@ user_input.grid(row=0,column=2)
 #Set Alarm Button
 
 start_button=tk.Button(box2,text="Set Alarm",font=('Arial Narrow',16,'bold'),command=set_alarm)
-
 start_button.grid(row=2,column=2)
 
 
